9320958876dde75bdcac148b613597d8b1c15761
Cezar Berea <berea.cezar@gmail.com>
2020-05-01 12:05:25 +0300

    Add render function pattern to T

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { render } from 'react-dom'
 
 import { I18nProvider, T, I18nService } from '../../src'
@@ -14,25 +14,71 @@ const i18nService = new I18nService(languages, {
 
 i18nService.loadDictionary('en', en)
 
-export default class Demo extends Component {
-  render() {
-    return (
-      <I18nProvider i18nService={i18nService}>
-        <div>
-          <h1>i18n Demo</h1>
-          <LanguageSwitcher />
-          <p>
-            Hello translated: <T label='hello' />
-          </p>
-          <div>
-            <button>
-              <T label='save' />
-            </button>
-          </div>
-        </div>
-      </I18nProvider>
-    )
-  }
+const Demo = () => {
+  const [lang, setLang] = useState('en')
+
+  return (
+    <I18nProvider i18nService={i18nService}>
+      <div>
+        <h1>i18n Demo</h1>
+        <h2>Language selector</h2>
+        <p>
+          This component allows you to have a way for the user to select a
+          language but not changing the UI language. To be used in forms.
+        </p>
+        <LanguageSelector onChange={setLang} value={lang} />
+        <p>Selected language: {lang}</p>
+        <h2>Language switcher</h2>
+        <p>This one changes the language across the app</p>
+        <LanguageSwitcher />
+        <h2>The T component</h2>
+        <p>Use this component to display translated strings</p>
+        <p>
+          The label <code>hello</code> doesn't have DE translations. It will
+          fallback to <em>en</em>. <br />
+          Hello translated:
+          <strong>
+            <T label='hello' />
+          </strong>
+        </p>
+        <p>
+          <button>
+            <T label='save' />
+          </button>
+        </p>
+        <h3>Render function</h3>
+        Sometimes you need the translation as a string to pass it to props like{' '}
+        <code>img.alt</code>
+        or other situations where a <code>ReactNode</code> just won't do.
+        <T>
+          {({ translate }) => (
+            <>
+              <p>
+                As a title for an anchor:{' '}
+                <a
+                  href='google.com'
+                  target='_blank'
+                  title={translate('hello-world')}>
+                  Hover me
+                </a>
+              </p>
+              <p>
+                As alt text for an image:
+                <img src='unknown' alt={translate('hello-world')} />
+              </p>
+            </>
+          )}
+        </T>
+        <h3>Display current language</h3>
+        <p>
+          You can also use <code>T</code> to display current language
+        </p>
+        <T>
+          {({ language }) => <p>Current language across app: {language}</p>}
+        </T>
+      </div>
+    </I18nProvider>
+  )
 }
 
 render(<Demo />, document.querySelector('#demo'))
--- src/T.jsx
+++ src/T.jsx
@@ -2,8 +2,17 @@ import { useContext, useMemo } from 'react'
 import PropTypes from 'prop-types'
 import { I18nContext } from './I18nProvider'
 
-export const T = ({ label, args }) => {
-  const { translate } = useContext(I18nContext)
+export const T = ({ label, args, children }) => {
+  const { translate, language, languages } = useContext(I18nContext)
+
+  if (children) {
+    if (typeof children === 'function') {
+      return children({ translate, language, languages })
+    } else {
+      console.warn('Invalid children passed. Only render function allowed')
+    }
+  }
+
   const value = useMemo(() => translate(label, args), [translate, args, label])
 
   return value
@@ -11,5 +20,6 @@ export const T = ({ label, args }) => {
 
 T.propTypes = {
   label: PropTypes.string,
-  args: PropTypes.oneOfType([PropTypes.array, PropTypes.object])
+  args: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
+  children: PropTypes.func
 }
